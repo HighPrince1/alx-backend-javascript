7368c8be5ef5293f4f6018564f822179b431d0ed
HighPrince1 <odomprincewill@gmail.com>
2023-06-30 08:46:06 +0000

    0x00-ES6_basic

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,14 +1,5 @@
-export default function createReportObject(employeesList) {
-  const allEmployees = {};
-
-  for (const [departmentName, employees] of Object.entries(employeesList)) {
-    allEmployees[departmentName] = [...employees];
-  }
-
+export default function createEmployeesObject(departmentName, employees) {
   return {
-    allEmployees,
-    getNumberOfDepartments() {
-      return Object.keys(employeesList).length;
-    },
+    [departmentName]: [...employees],
   };
 }
